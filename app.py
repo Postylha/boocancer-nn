e0a8028364d4256e09a4ab0d17f2a05dece565bb
Postylha <114649888+Postylha@users.noreply.github.com>
2024-04-17 11:26:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import base64
 from io import BytesIO
@@ -9,13 +10,11 @@ import os
 
 app = Flask(__name__)
 
-
-# Function to preprocess the image for your model
 def preprocess_image(img, target_size=(224, 224)):
     if img.mode != "RGB":
         img = img.convert("RGB")
     img = img.resize(target_size)
-    img_array = image.img_to_array(img)
+    img_array = img_to_array(img)
     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
     return img_array_expanded_dims
 
@@ -42,13 +41,17 @@ def test():
     processed_image = preprocess_image(img)
 
     # Make a prediction
+    result = None
     prediction = model.predict(processed_image)
-
+    if prediction > 0.5:
+        result = 1
+    else:
+        result = 0
+    
     # Return the prediction in JSON format
-    response_data = {'prediction': prediction.tolist()}  # Convert numpy array to list for JSON serialization
+    response_data = {'prediction': result}  # Convert numpy array to list for JSON serialization
 
     return jsonify(response_data), 200
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
